f54344abdec7ec33e962bc0f165635e2ba46e0d9
Sergey Dimitriev <serdim@MacBook-Pro-Sergey.local>
2019-07-31 20:41:48 +0400

    Initial Commit

---
+++ menu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  menu
+//
+//  Created by Sergey Dimitriev on 31/07/2019.
+//  Copyright © 2019 Sergey Dimitriev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
