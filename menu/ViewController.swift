fd4991babdafd1c9b2bfe69b30cb0d3734c047da
Sergey Dimitriev <serdim@MacBook-Pro-Sergey.local>
2019-08-01 21:35:38 +0400

    First commit

--- menu/ViewController.swift
+++ menu/ViewController.swift
@@ -9,12 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var btnDrop: UIButton!
+    @IBOutlet weak var tblView: UITableView!
+    
+    var fruitList = ["Orange", "Banana", "Apple", "Blueberry", "Mango", "Cherry", "Grape", "Strawberry"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tblView.isHidden = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onClickDropButton(_ sender: Any) {
+        if tblView.isHidden {
+            animate(toogle: true, type: btnDrop)
+        } else {
+            animate(toogle: false, type: btnDrop)
+        }
+    }
+    func animate(toogle: Bool, type: UIButton) {
+        
+        if type == btnDrop {
+            
+            if toogle {
+                UIView.animate(withDuration: 0.3) {
+                    self.tblView.isHidden = false
+                }
+            } else {
+                UIView.animate(withDuration: 0.3) {
+                    self.tblView.isHidden = true
+                }
+            }
+        }
+    }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruitList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = fruitList[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        btnDrop.setTitle("\(fruitList[indexPath.row])", for: .normal)
+        animate(toogle: false, type: btnDrop)
+    }
+    
+    
+}
